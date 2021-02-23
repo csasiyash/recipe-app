7869a73543fd2fe8754d18ff78c72da99780fac1
csasiyash <padiayash98@gmail.com>
2021-02-23 19:58:25 +0530

    small change

--- components/HeaderButton.js
+++ components/HeaderButton.js
@@ -3,7 +3,7 @@ import {HeaderButton} from 'react-navigation-header-buttons';
 import {Ionicons} from 'react-native-vector-icons';
 import Colors from '../constants/Colors';
 
-//By returning {...props} we are passing all the key value pairs to headrButton
+//By  returning {...props} we are passing all the key value pairs to headrButton
 const CustonHeaderButton = (props) => {
   return (
     <HeaderButton
